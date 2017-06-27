6cf88f3878804d42fcb833223123acdeb0d0f48c
Quang <quangdx87@gmail.com>
2017-06-27 10:23:20 +0700

    update

--- js/custom.js
+++ js/custom.js
@@ -2,21 +2,29 @@
 $(document).ready(function(){
   $('.scrolltop').click(function() {
     $('html, body').animate({scrollTop: 0}, 800);
-  return false;
-    });
+    return false;
+  });
 
-    	$("#datepicker").datepicker();
+  $("#datepicker").datepicker();
 
-    $(".close-btn").click(function() {
-      var height =  $(".showcase .col-1").height() - 6;
-      $(".box-search").toggle();
-      $(".showcase .col-2").css({'width': '54%', 'height' : height, 'overflow' : 'hidden'});
-    });
+  $(".close-btn").click(function() {
+    var height =  $(".showcase .col-1").height() - 6;
+    $(".box-search").toggle();
+    $(".showcase .col-2").css({'width': '54%', 'height' : height, 'overflow' : 'hidden'});
+  });
 
-  	$("#open-menu").click(function() {
-        $("body").toggleClass('menu-open');
-  	});
+  $("#open-menu").click(function() {
+    $("body").toggleClass('menu-open');
+  });
 
+
+  $(window).scroll(function(){
+    var sticky = $('#header'),
+    scroll = $(window).scrollTop();
+
+    if (scroll >= 100) sticky.addClass('fixed');
+    else sticky.removeClass('fixed');
+  });
 
 
 });
